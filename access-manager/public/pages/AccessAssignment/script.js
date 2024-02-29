01f22075143e39a8fff0614748f8b8dce67b8c4d
Dario Casciato <dario.casciato@dormakaba.com>
2024-02-29 10:19:27 +0100

    Fix date formatting in script.js

--- access-manager/public/pages/AccessAssignment/script.js
+++ access-manager/public/pages/AccessAssignment/script.js
@@ -80,7 +80,7 @@ function fetchAssignments() {
 function formatDate(dateString) {
     const date = new Date(dateString);
     const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // JavaScript months are 0-based.
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const year = date.getFullYear();
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
